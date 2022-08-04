421238f043417a584ff17d53660b765fe33fdbfe
praveen dodla <dodlapraveen09@gmail.com>
2022-08-04 13:09:32 +0530

    Ability to add multiple person to Address Book

---
+++ addressbook/addressbook/AddressBookBuilder.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook
+{
+    class AddressBookBuilder : IContacts
+    {
+        public Dictionary<string, Contacts> dictionary;
+        public AddressBookBuilder()
+        {
+            this.dictionary = new Dictionary<string, Contacts>();
+        }
+        public void addContact()
+        {
+            string FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email;
+            Console.WriteLine("Enter First Name : ");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address: ");
+            Address = Console.ReadLine();
+            Console.WriteLine("Enter City : ");
+            City = Console.ReadLine();
+            Console.WriteLine("Enter State : ");
+            State = Console.ReadLine();
+            Console.WriteLine("Enter Zip code : ");
+            Zip = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number : ");
+            PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            Email = Console.ReadLine();
+            Contacts contact = new Contacts(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email);
+            dictionary.Add(FirstName, contact);
+        }
+        public void displayContacts()
+        {
+            foreach (KeyValuePair<string, Contacts> dict in dictionary)
+            {
+                Console.WriteLine("First Name : " + dict.Value.firstName);
+                Console.WriteLine("Last Name : " + dict.Value.lastName);
+                Console.WriteLine("Address : " + dict.Value.address);
+                Console.WriteLine("City : " + dict.Value.city);
+                Console.WriteLine("State : " + dict.Value.state);
+                Console.WriteLine("Zip : " + dict.Value.zip);
+                Console.WriteLine("Phone Number : " + dict.Value.phoneNo);
+                Console.WriteLine("Email : " + dict.Value.email);
+            }
+        }
+        public void editContact(string firstName)
+        {
+            int found = 0;
+            foreach (KeyValuePair<string, Contacts> dict in dictionary)
+            {
+                if (firstName.Equals(dict.Key))
+                {
+                    found = 1;
+                    Console.WriteLine("Enter Last Name : ");
+                    dict.Value.lastName = Console.ReadLine();
+                    Console.WriteLine("Enter Address: ");
+                    dict.Value.address = Console.ReadLine();
+                    Console.WriteLine("Enter City : ");
+                    dict.Value.city = Console.ReadLine();
+                    Console.WriteLine("Enter State : ");
+                    dict.Value.state = Console.ReadLine();
+                    Console.WriteLine("Enter Zip code : ");
+                    dict.Value.zip = Console.ReadLine();
+                    Console.WriteLine("Enter Phone Number : ");
+                    dict.Value.phoneNo = Console.ReadLine();
+                    Console.WriteLine("Enter Email : ");
+                    dict.Value.email = Console.ReadLine();
+                }
+            }
+            if (found == 0)
+            {
+                Console.WriteLine("No Contacts matched with given first name");
+            }
+        }
+        public void deleteContact(string firstName)
+        {
+            int found = 0;
+            foreach (KeyValuePair<string, Contacts> dict in dictionary)
+            {
+                if (firstName.Equals(dict.Key))
+                {
+                    dictionary.Remove(firstName);
+                    found = 1;
+                    Console.WriteLine("Contact deleted");
+                }
+            }
+            if (found == 0)
+            {
+                Console.WriteLine("No Contacts matched with given first name");
+            }
+        }
+    }
+}
--- addressbook/addressbook/Program.cs
+++ addressbook/addressbook/Program.cs
@@ -6,22 +6,55 @@ namespace addressbook
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Welcome To Address Book Program!");
-            //ContactDetails createContacts = new ContactDetails();
-            //createContacts.Contacts();
+            Console.WriteLine("Welcome to Address Book System");
+            AddressBookBuilder addressBookBuilder = new AddressBookBuilder();
+            while (true)
+            {
+                Console.WriteLine("1. Add Contact\n2. Display Contacts\n3. Edit Contact \n4.Delete Contact\n5.Add multiple contacts\n6.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressBookBuilder.addContact();
+                        break;
+                    case 2:
+                        addressBookBuilder.displayContacts();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the first name of the contact that need to be changed");
+                        String firstNameSearch = Console.ReadLine();
+                        addressBookBuilder.editContact(firstNameSearch);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the first name of the contact that need to be deleted");
+                        String firstName = Console.ReadLine();
+                        addressBookBuilder.deleteContact(firstName);
+                        break;
+                    case 5:
+                        addressBookBuilder.addContact();
+                        while (true)
+                        {
+                            Console.WriteLine("Want to add another contact(Y/N)");
+                            char ans = char.ToUpper(Console.ReadLine()[0]);
+                            if (ans.Equals('Y'))
+                            {
+                                addressBookBuilder.addContact();
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        break;
+                    case 6:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter Valid Choice");
+                        break;
+                }
 
-            //Console.WriteLine("Add New Contacts Program");
-            //AddNewContacts addNewCon = new AddNewContacts();
-            //addNewCon.addNewContacts();
-
-            //Console.WriteLine("Update Existing Contact Program");
-            //UpdateExistingContact update = new UpdateExistingContact();
-            //update.UpdateContact();
-
-            Console.WriteLine("Delete a Contact Program");
-            DeleteContact deleteContact = new DeleteContact();
-            deleteContact.ContactDelete();
-            Console.ReadKey();
+            }
         }
     }
 }
