4de12ed9cc57bcb34eff802246a7084afeb78011
praveen dodla <dodlapraveen09@gmail.com>
2022-08-04 10:53:38 +0530

    create a Contacts in Address Book

---
+++ addressbook/addressbook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace addressbook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program!");
+            ContactDetails createContacts = new ContactDetails();
+            createContacts.Contacts();
+        }
+    }
+}
