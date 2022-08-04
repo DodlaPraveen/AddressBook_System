ce107b582032cd6f9a827a3ee5c442ef2bfefca5
praveen dodla <dodlapraveen09@gmail.com>
2022-08-04 11:15:00 +0530

    Ability to add a new Contact

--- addressbook/addressbook/Program.cs
+++ addressbook/addressbook/Program.cs
@@ -9,6 +9,10 @@ namespace addressbook
             Console.WriteLine("Welcome To Address Book Program!");
             ContactDetails createContacts = new ContactDetails();
             createContacts.Contacts();
+
+            Console.WriteLine("Add New Contacts Program");
+            AddNewContacts addNewCon = new AddNewContacts();
+            addNewCon.addNewContacts();
         }
     }
 }
