24d91e99210b13367c824d69d5bbdcddea52cca7
praveen dodla <dodlapraveen09@gmail.com>
2022-09-03 17:09:18 +0530

    sort by city, state and zip code

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public List<Contact> contactList;
+
+
+        public AddressBook()
+        {
+            contactList = new List<Contact>();
+
+        }
+        public string AddContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            if (CheckName(firstName, lastName) == false)
+            {
+                Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNo, emailId);
+                contactList.Add(contact);
+                return "Details Added Successfully";
+            }
+            return "Name already exists";
+        }
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipCode, string phoneNo, string emailId)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName))
+                {
+                    c.lastName = lastName;
+                    c.address = address;
+                    c.city = city;
+                    c.state = state;
+                    c.zipCode = zipCode;
+                    c.phoneNo = phoneNo;
+                    c.emailId = emailId;
+
+                    return;
+                }
+            }
+        }
+        public void RemoveContact(string firstName, string lastName)
+        {
+            foreach (Contact c in contactList)
+            {
+                if (c.firstName.Equals(firstName) && c.lastName.Equals(lastName))
+                {
+                    contactList.Remove(c);
+
+                    return;
+                }
+            }
+        }
+        public bool CheckName(string firstName, string lastName)
+        {
+            foreach (Contact c in contactList.FindAll(e=> e.firstName.Equals(firstName) && e.lastName.Equals(lastName)))
+            {
+                return true;                
+            }
+            return false;
+        }
+        public List<Contact> GetPersonByCityOrState(string cityOrState)
+        {
+            List<Contact> contact = new List<Contact>();
+            foreach (Contact c in contactList.FindAll(e=> e.city.Equals(cityOrState) || e.state.Equals(cityOrState)))
+            {               
+                    contact.Add(c);
+            }
+            return contact;
+        }
+        public void SortByName()
+        {
+            contactList.Sort((contact1, contact2) => contact1.firstName.CompareTo(contact2.firstName));
+            foreach (Contact c in contactList)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+        public void SortByCity()
+        {
+            contactList.Sort((contact1, contact2) => contact1.city.CompareTo(contact2.city));
+            foreach (Contact c in contactList)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+        public void SortByState()
+        {
+            contactList.Sort((contact1, contact2) => contact1.state.CompareTo(contact2.state));
+            foreach (Contact c in contactList)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+        public void SortByZipCode()
+        {
+            contactList.Sort((contact1, contact2) => contact1.zipCode.CompareTo(contact2.zipCode));
+            foreach (Contact c in contactList)
+            {
+                Console.WriteLine(c.ToString());
+            }
+        }
+    }
+}
